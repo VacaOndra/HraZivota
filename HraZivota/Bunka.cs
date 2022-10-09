9a36713c83cf65dfe255b384dda5ebb3b2ce2720
VacaOndra <vacek.ondra319@gmail.com>
2022-10-09 17:14:49 +0200

    Přidejte soubory projektu.

---
+++ HraZivota/Bunka.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace HraZivota
+{
+    public enum Stavy
+    {
+        Ziva,
+        Mrtva
+    }
+    class Bunka
+    {
+        public Stavy PredchoziStav { get; set; }
+        public Stavy AktualniStav { get { return aktualniStav; } set { aktualniStav = value; ZmenBarvuBunky(); } }
+        private Stavy aktualniStav;
+        private Color barvaBunky = Color.FromRgb(73, 158, 191);
+        private Random random = new Random();
+        public Rectangle rectangle;
+        public Bunka()
+        {
+            rectangle = new Rectangle();
+            //rectangle.StrokeThickness = 0.5;
+            //rectangle.Stroke = Brushes.Gray;
+            if (random.Next(5) == 1)
+            {
+                PredchoziStav = Stavy.Ziva;
+                AktualniStav = Stavy.Ziva;
+            } 
+            else
+            {
+                PredchoziStav = Stavy.Mrtva;
+                AktualniStav = Stavy.Mrtva;
+            }
+                
+        }
+        private void ZmenBarvuBunky()
+        {
+            rectangle.Fill = (AktualniStav == Stavy.Ziva) ? new SolidColorBrush(barvaBunky) : Brushes.Transparent;
+        }
+    }
+}
---
+++ HraZivota/MainWindow.xaml.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace HraZivota
+{
+    public partial class MainWindow : Window
+    {
+        private Bunka[,] poleBunek;
+        DispatcherTimer timer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            NaplPole(160,90);
+            timer = new DispatcherTimer();
+            timer.Tick += Timer_Tick;
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 200);
+            
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            PrepocitejBunky();
+        }
+
+        private void NaplPole(int sirka, int vyska)
+        {
+            poleBunek = new Bunka[sirka, vyska];
+
+            for (int i = 0; i < poleBunek.GetLength(0); i++)
+            {
+                for (int j = 0; j < poleBunek.GetLength(1); j++)
+                {
+                    poleBunek[i, j] = new Bunka();
+                }
+            }
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            VykresliPlatno();
+            timer.Start();
+        }
+        private void VykresliPlatno()
+        {
+            canvas.Children.Clear();
+            double sire = canvas.ActualWidth / poleBunek.GetLength(0);
+            double vyska = canvas.ActualHeight / poleBunek.GetLength(1);
+            for (int i = 0; i < poleBunek.GetLength(0); i++)
+            {
+                for (int j = 0; j < poleBunek.GetLength(1); j++)
+                {
+                    poleBunek[i, j].rectangle.Width = sire;
+                    poleBunek[i, j].rectangle.Height = vyska;
+                    Canvas.SetTop(poleBunek[i, j].rectangle, vyska * j);
+                    Canvas.SetLeft(poleBunek[i, j].rectangle, sire * i);
+                    canvas.Children.Add(poleBunek[i, j].rectangle);
+                }
+            }
+        }
+        private void PrepocitejBunky()
+        {
+            for (int i = 0; i < poleBunek.GetLength(0); i++)
+            {
+                for (int j = 0; j < poleBunek.GetLength(1); j++)
+                {
+                    poleBunek[i, j].PredchoziStav = poleBunek[i, j].AktualniStav;
+                    int ziveBunky = 0;
+                    //nahore vlevo
+                    if (i > 0 && j > 0)
+                    {
+                        if (poleBunek[i - 1, j - 1].PredchoziStav == Stavy.Ziva)
+                        {
+                            ziveBunky++;
+                        }
+                    }
+                    //nahore
+                    if (j > 0)
+                    {
+                        if (poleBunek[i, j - 1].PredchoziStav == Stavy.Ziva)
+                        {
+                            ziveBunky++;
+                        }
+                    }
+                    //nahore vpravo
+                    if (i < poleBunek.GetLength(0) - 1 && j > 0)
+                    {
+                        if (poleBunek[i + 1, j - 1].PredchoziStav == Stavy.Ziva)
+                        {
+                            ziveBunky++;
+                        }
+                    }
+                    //vlevo
+                    if (i > 0)
+                    {
+                        if (poleBunek[i - 1, j].PredchoziStav == Stavy.Ziva)
+                        {
+                            ziveBunky++;
+                        }
+                    }
+                    //vpravo
+                    if (i < poleBunek.GetLength(0) - 1)
+                    {
+                        if (poleBunek[i + 1, j].PredchoziStav == Stavy.Ziva)
+                        {
+                            ziveBunky++;
+                        }
+                    }
+                    //dole vlevo
+                    if (i > 0 && j < poleBunek.GetLength(1) - 1)
+                    {
+                        if (poleBunek[i - 1, j + 1].PredchoziStav == Stavy.Ziva)
+                        {
+                            ziveBunky++;
+                        }
+                    }
+                    //dole
+                    if (j < poleBunek.GetLength(1) - 1)
+                    {
+                        if (poleBunek[i, j + 1].PredchoziStav == Stavy.Ziva)
+                        {
+                            ziveBunky++;
+                        }
+                    }
+                    //dole vpravo
+                    if (i < poleBunek.GetLength(0) - 1 && j < poleBunek.GetLength(1) - 1)
+                    {
+                        if (poleBunek[i + 1, j + 1].PredchoziStav == Stavy.Ziva)
+                        {
+                            ziveBunky++;
+                        }
+                    }
+                    if (poleBunek[i, j].AktualniStav == Stavy.Ziva && ziveBunky < 2)
+                    {
+                        poleBunek[i, j].AktualniStav = Stavy.Mrtva;
+                    }
+                    else if (poleBunek[i, j].AktualniStav == Stavy.Ziva && ziveBunky >= 2 && ziveBunky <= 3)
+                    {
+                        poleBunek[i, j].AktualniStav = Stavy.Ziva;
+                    }
+                    else if (poleBunek[i, j].AktualniStav == Stavy.Ziva && ziveBunky > 3)
+                    {
+                        poleBunek[i, j].AktualniStav = Stavy.Mrtva;
+                    }
+                    else if (poleBunek[i, j].AktualniStav == Stavy.Mrtva && ziveBunky == 3)
+                    {
+                        poleBunek[i, j].AktualniStav = Stavy.Ziva;
+                    }
+                }
+            }
+        }
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            VykresliPlatno();
+        }
+    }
+}
